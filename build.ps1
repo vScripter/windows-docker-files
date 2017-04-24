f2a72a31a91ae06f7b30d92cfe98b86c269c8e99
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-04-24 15:00:38 -0400

    Settup up CI
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

---
+++ build.ps1
@@ -0,0 +1,50 @@
+<#
+    Borrowed with gratitude from Stefan Scherer
+    https://github.com/StefanScherer/dockerfiles-windows/blob/master
+#>
+
+$ErrorActionPreference = 'Stop';
+$files = ""
+Write-Host Starting build
+
+Write-Host Updating base images
+docker pull microsoft/windowsservercore
+docker pull microsoft/nanoserver
+
+if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
+  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
+  $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
+} else {
+  Write-Host Branch $env:APPVEYOR_REPO_BRANCH
+  $files = $(git diff --name-only HEAD~1)
+}
+
+Write-Host Changed files:
+
+$dirs = @{}
+
+$files | ForEach-Object {
+  Write-Host $_
+  $dir = $_ -replace "\/[^\/]+$", ""
+  $dir = $dir -replace "/", "\"
+  if (Test-Path "$dir\build.ps1") {
+    Write-Host "Storing $dir for build"
+    $dirs.Set_Item($dir, 1)
+  } else {
+    $dir = $dir -replace "\\[^\\]+$", ""
+    if (Test-Path "$dir\build.ps1") {
+      Write-Host "Storing $dir for build"
+      $dirs.Set_Item($dir, 1)
+    }
+  }
+}
+
+$dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
+  $dir = $_.Name
+  Write-Host Building in directory $dir
+  pushd $dir
+  .\build.ps1
+  popd
+}
+
+docker images
---
+++ test.ps1
@@ -0,0 +1,44 @@
+<#
+    Borrowed with gratitude from Stefan Scherer
+    https://github.com/StefanScherer/dockerfiles-windows/blob/master
+#>
+
+$ErrorActionPreference = 'Stop';
+$files = ""
+Write-Host Starting test
+
+if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
+  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
+  $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
+} else {
+  Write-Host Branch $env:APPVEYOR_REPO_BRANCH
+  $files = $(git diff --name-only HEAD~1)
+}
+
+Write-Host Changed files:
+
+$dirs = @{}
+
+$files | ForEach-Object {
+  Write-Host $_
+  $dir = $_ -replace "\/[^\/]+$", ""
+  $dir = $dir -replace "/", "\"
+  if (Test-Path "$dir\test.ps1") {
+    Write-Host "Storing $dir for test"
+    $dirs.Set_Item($dir, 1)
+  } else {
+    $dir = $dir -replace "\\[^\\]+$", ""
+    if (Test-Path "$dir\test.ps1") {
+      Write-Host "Storing $dir for test"
+      $dirs.Set_Item($dir, 1)
+    }
+  }
+}
+
+$dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
+  $dir = $_.Name
+  Write-Host Testing in directory $dir
+  pushd $dir
+  .\test.ps1
+  popd
+}
