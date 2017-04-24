c39f85a8c096416ee92d3356c2b48c723f41d237
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-04-24 15:10:53 -0400

    .gitattributes file update; build.ps1 minor update
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ Write-Host Starting build
 
 Write-Host Updating base images
 docker pull microsoft/windowsservercore
-docker pull microsoft/nanoserver
+#docker pull microsoft/nanoserver
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
   Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
