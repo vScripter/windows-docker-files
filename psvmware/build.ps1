fa40e7ffa5537762020879ef0ed24e5e7eef96c5
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-04-24 15:35:55 -0400

    Commented out nanoserver image from build.ps1
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- psvmware/build.ps1
+++ psvmware/build.ps1
@@ -3,7 +3,7 @@ Write-Host Starting build
 
 Write-Host Updating base images
 docker pull microsoft/windowsservercore
-docker pull microsoft/nanoserver
+#docker pull microsoft/nanoserver
 
 docker build -t vscripter/psvmware .
 
