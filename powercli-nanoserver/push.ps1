8e199f90d65306bb1e0b7a5cc622dd2d7e583ba1
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-04-25 13:54:28 -0400

    more fixes
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- powercli-nanoserver/push.ps1
+++ powercli-nanoserver/push.ps1
@@ -1,2 +1,2 @@
-docker tag powercli-windowsservercore vscripter/powercli-nanoserver:latest
+docker tag powercli-nanoserver vscripter/powercli-nanoserver:latest
 docker push vscripter/powercli-nanoserver:latest
