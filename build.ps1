a4aa20a77794e8602527109d3eb7802bcfea875c
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-04-25 13:40:31 -0400

    Added color to write-host build file commands
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- build.ps1
+++ build.ps1
@@ -5,35 +5,35 @@
 
 $ErrorActionPreference = 'Stop';
 $files = ""
-Write-Host Starting build
+Write-Host Starting build -ForegroundColor 'Yellow'
 
-Write-Host Updating base images
+Write-Host Updating base images -ForegroundColor 'Yellow'
 docker pull microsoft/windowsservercore
 #docker pull microsoft/nanoserver
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
-  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
+  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER -ForegroundColor 'Yellow'
   $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
 } else {
-  Write-Host Branch $env:APPVEYOR_REPO_BRANCH
+  Write-Host Branch $env:APPVEYOR_REPO_BRANCH -ForegroundColor 'Yellow'
   $files = $(git diff --name-only HEAD~1)
 }
 
-Write-Host Changed files:
+Write-Host Changed files: -ForegroundColor 'Yellow'
 
 $dirs = @{}
 
 $files | ForEach-Object {
-  Write-Host $_
+  Write-Host $_ -ForegroundColor 'Yellow'
   $dir = $_ -replace "\/[^\/]+$", ""
   $dir = $dir -replace "/", "\"
   if (Test-Path "$dir\build.ps1") {
-    Write-Host "Storing $dir for build"
+    Write-Host "Storing $dir for build" -ForegroundColor 'Yellow'
     $dirs.Set_Item($dir, 1)
   } else {
     $dir = $dir -replace "\\[^\\]+$", ""
     if (Test-Path "$dir\build.ps1") {
-      Write-Host "Storing $dir for build"
+      Write-Host "Storing $dir for build" -ForegroundColor 'Yellow'
       $dirs.Set_Item($dir, 1)
     }
   }
@@ -41,10 +41,10 @@ $files | ForEach-Object {
 
 $dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
   $dir = $_.Name
-  Write-Host Building in directory $dir
-  pushd $dir
+  Write-Host Building in directory $dir -ForegroundColor 'Yellow'
+  Push-Location $dir
   .\build.ps1
-  popd
+  Pop-Location
 }
 
 docker images
--- test.ps1
+++ test.ps1
@@ -5,31 +5,31 @@
 
 $ErrorActionPreference = 'Stop';
 $files = ""
-Write-Host Starting test
+Write-Host Starting test -ForegroundColor 'Yellow'
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
-  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
+  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER -ForegroundColor 'Yellow'
   $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
 } else {
-  Write-Host Branch $env:APPVEYOR_REPO_BRANCH
+  Write-Host Branch $env:APPVEYOR_REPO_BRANCH -ForegroundColor 'Yellow'
   $files = $(git diff --name-only HEAD~1)
 }
 
-Write-Host Changed files:
+Write-Host Changed files: -ForegroundColor 'Yellow'
 
 $dirs = @{}
 
 $files | ForEach-Object {
-  Write-Host $_
+  Write-Host $_ -ForegroundColor 'Yellow'
   $dir = $_ -replace "\/[^\/]+$", ""
   $dir = $dir -replace "/", "\"
   if (Test-Path "$dir\test.ps1") {
-    Write-Host "Storing $dir for test"
+    Write-Host "Storing $dir for test" -ForegroundColor 'Yellow'
     $dirs.Set_Item($dir, 1)
   } else {
     $dir = $dir -replace "\\[^\\]+$", ""
     if (Test-Path "$dir\test.ps1") {
-      Write-Host "Storing $dir for test"
+      Write-Host "Storing $dir for test" -ForegroundColor 'Yellow'
       $dirs.Set_Item($dir, 1)
     }
   }
@@ -37,8 +37,8 @@ $files | ForEach-Object {
 
 $dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
   $dir = $_.Name
-  Write-Host Testing in directory $dir
-  pushd $dir
+  Write-Host Testing in directory $dir -ForegroundColor 'Yellow'
+  Push-Location $dir
   .\test.ps1
-  popd
+  Pop-Location
 }
