6f6d0e852d4ef460fab0844f17e8762b193dffcc
Kevin Kirkpatrick <kevin@nullzero.io>
2018-07-10 09:08:39 -0400

    Updated deploy.ps1
    Signed-off-by: Kevin Kirkpatrick <kevin@nullzero.io>

--- deploy.ps1
+++ deploy.ps1
@@ -6,23 +6,23 @@
 $ErrorActionPreference = 'Stop';
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER -Or ! $env:APPVEYOR_REPO_BRANCH.Equals("master")) {
-  Write-Host Nothing to deploy. -ForegroundColor 'Yellow'
+  Write-Host "Nothing to deploy." -ForegroundColor 'Yellow'
   Exit 0
 }
 
 $files = ""
-Write-Host Starting deploy -ForegroundColor 'Yellow'
-docker login --username="$env:DOCKER_USER" --password-stdin="$env:DOCKER_PASS"
+Write-Host "Starting deploy" -ForegroundColor 'Yellow'
+docker login --username="$env:DOCKER_USER" --password="$env:DOCKER_PASS"
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
-  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER -ForegroundColor 'Yellow'
+  Write-Host "Pull request $($env:APPVEYOR_PULL_REQUEST_NUMBER)" -ForegroundColor 'Yellow'
   $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
 } else {
-  Write-Host Branch $env:APPVEYOR_REPO_BRANCH -ForegroundColor 'Yellow'
+  Write-Host "Branch $($env:APPVEYOR_REPO_BRANCH)" -ForegroundColor 'Yellow'
   $files = $(git diff --name-only HEAD~1)
 }
 
-Write-Host Changed files: -ForegroundColor 'Yellow'
+Write-Host "Changed files:" -ForegroundColor 'Yellow'
 
 $dirs = @{}
 
@@ -31,12 +31,12 @@ $files | ForEach-Object {
   $dir = $_ -replace "\/[^\/]+$", ""
   $dir = $dir -replace "/", "\"
   if (Test-Path "$dir\push.ps1") {
-    Write-Host "Storing $dir for deployment" -ForegroundColor 'Yellow'
+    Write-Host "Storing $($dir) for deployment" -ForegroundColor 'Yellow'
     $dirs.Set_Item($dir, 1)
   } else {
     $dir = $dir -replace "\\[^\\]+$", ""
     if (Test-Path "$dir\push.ps1") {
-      Write-Host "Storing $dir for deployment" -ForegroundColor 'Yellow'
+      Write-Host "Storing $($dir) for deployment" -ForegroundColor 'Yellow'
       $dirs.Set_Item($dir, 1)
     }
   }
@@ -44,7 +44,7 @@ $files | ForEach-Object {
 
 $dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
   $dir = $_.Name
-  Write-Host Building in directory $dir -ForegroundColor 'Yellow'
+  Write-Host "Building in directory $($dir)" -ForegroundColor 'Yellow'
   Push-Location $dir
   .\push.ps1
   Pop-Location
