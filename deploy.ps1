38a4432ee6f86f59b75953ceee08a2b324363c2a
Kevin Kirkpatrick <kevin@nullzero.io>
2018-12-04 23:41:50 -0500

    Updated docker hub auth in deploy.ps1
    Signed-off-by: Kevin Kirkpatrick <kevin@nullzero.io>

--- deploy.ps1
+++ deploy.ps1
@@ -6,37 +6,50 @@
 $ErrorActionPreference = 'Stop';
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER -Or ! $env:APPVEYOR_REPO_BRANCH.Equals("master")) {
-  Write-Host "Nothing to deploy." -ForegroundColor 'Yellow'
+  Write-Host Nothing to deploy.
   Exit 0
 }
 
 $files = ""
-Write-Host "Starting deploy" -ForegroundColor 'Yellow'
-docker login --username="$env:DOCKER_USER" --password-stdin
+Write-Host Starting deploy
+# "$env:DOCKER_PASS" | docker login --username "$env:DOCKER_USER" --password-stdin
+# docker login with the old config.json style that is needed for manifest-tool
+$auth =[System.Text.Encoding]::UTF8.GetBytes("$($env:DOCKER_USER):$($env:DOCKER_PASS)")
+$auth64 = [Convert]::ToBase64String($auth)
+@"
+{
+  "auths": {
+    "https://index.docker.io/v1/": {
+      "auth": "$auth64"
+    }
+  }
+}
+"@ | Out-File -Encoding Ascii ~/.docker/config.json
+
 
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
-  Write-Host "Pull request $($env:APPVEYOR_PULL_REQUEST_NUMBER)" -ForegroundColor 'Yellow'
+  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
   $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
 } else {
-  Write-Host "Branch $($env:APPVEYOR_REPO_BRANCH)" -ForegroundColor 'Yellow'
+  Write-Host Branch $env:APPVEYOR_REPO_BRANCH
   $files = $(git diff --name-only HEAD~1)
 }
 
-Write-Host "Changed files:" -ForegroundColor 'Yellow'
+Write-Host Changed files:
 
 $dirs = @{}
 
 $files | ForEach-Object {
-  Write-Host $_ -ForegroundColor 'Yellow'
+  Write-Host $_
   $dir = $_ -replace "\/[^\/]+$", ""
   $dir = $dir -replace "/", "\"
   if (Test-Path "$dir\push.ps1") {
-    Write-Host "Storing $($dir) for deployment" -ForegroundColor 'Yellow'
+    Write-Host "Storing $dir for deployment"
     $dirs.Set_Item($dir, 1)
   } else {
     $dir = $dir -replace "\\[^\\]+$", ""
     if (Test-Path "$dir\push.ps1") {
-      Write-Host "Storing $($dir) for deployment" -ForegroundColor 'Yellow'
+      Write-Host "Storing $dir for deployment"
       $dirs.Set_Item($dir, 1)
     }
   }
@@ -44,8 +57,8 @@ $files | ForEach-Object {
 
 $dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
   $dir = $_.Name
-  Write-Host "Building in directory $($dir)" -ForegroundColor 'Yellow'
-  Push-Location $dir
+  Write-Host Building in directory $dir
+  pushd $dir
   .\push.ps1
-  Pop-Location
+  popd
 }
